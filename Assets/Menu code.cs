ebaa8c5e1e99066b5c2d765afed1886e3af2fee0
Grayethecolour <andersengraye@gmail.com>
2024-09-21 17:30:04 -0600

    made start Button go to game

--- Assets/Menu code.cs
+++ Assets/Menu code.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menucode : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public void startGame()
     {
+        SceneManager.LoadScene(1); 
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
