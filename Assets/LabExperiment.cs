ca2f7f59d8f886108aba0bd308ad4243bd53fe4d
Shidhartha Sen <shidhartha98619@gmail.com>
2024-09-21 13:29:02 -0600

    fixed Error

--- Assets/LabExperiment.cs
+++ Assets/LabExperiment.cs
@@ -8,7 +8,7 @@ public class LabExperiment : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Rigidbody2D>().velocity = new Vector3(GameManager.Instance.speed, 0, 0);
+        GetComponent<Rigidbody2D>().velocity = new Vector3(GameManager.Instance.enemySpeed, 0, 0);
     }
 
     // Update is called once per frame
